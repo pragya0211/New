3c6c95546a1e9925776d1fb249070533e3a9e057
pragya0211 <111433494+pragya0211@users.noreply.github.com>
2023-11-01 23:30:54 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -44,7 +44,26 @@ pipeline {
         steps {
           script {
             withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'my-aws-credential']]) {
-              sh 'aws cloudfront create-invalidation --distribution-id ${INVALIDATION_ID} --paths ${PATHS}'
+              cmd = "aws cloudfront list-distributions | jq '.DistributionList.Items[]|[ .Id, .Status, .Origins.Items[0].DomainName, .Aliases.Items[0] ] | @tsv ' -r"
+            def list = sh(script: cmd, returnStdout: true)
+            echo list 
+
+            writeFile file: 'cloudfront_list.txt', text: list 
+            if(params.domain){
+              // get distribution id
+              get_cmd = """grep ${params.domain} 'cloudfront_list.txt' | awk '{print \$1}'"""
+              DISTRIBUTION_ID = sh(script: get_cmd, returnStdout: true)
+              println "$key = $DISTRIBUTION_ID"
+
+              // create invalidation id
+              create_cmd = """aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" | jq -r .Invalidation.Id"""
+              echo create_cmd 
+              INVALIDATION_ID = sh(script: create_cmd, returnStdout: true)
+
+              // invalidate distribution and wait for finish
+              wait_cmd = """aws cloudfront wait invalidation-completed --distribution-id $DISTRIBUTION_ID --id $INVALIDATION_ID"""
+              sh(wait_cmd)
+              // sh 'aws cloudfront create-invalidation --distribution-id ${INVALIDATION_ID} --paths ${PATHS}'
             }
           }
         }
